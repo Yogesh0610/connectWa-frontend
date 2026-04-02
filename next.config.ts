2cf8281b07738d9120ee024aef69ecd2a892a7ed
Yogesh Kumar <yogeshkumar@Yogeshs-MacBook-Air.local>
2026-04-03 02:43:12 +0530

    Update Next.js config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_API_URL: "https://your-backend-domain.com/api",
-    NEXT_PUBLIC_API_BASE_URL: "/api",
-    NEXT_PUBLIC_STORAGE_URL: "https://your-backend-domain.com/",
-  },
+  env: {},
   images: {
     remotePatterns: [
       {
